9bbd0e401d4cbe68841fdab8d1c4a586dbfc9fc9
Ashu2356 <asavariparit29@gmail.com>
2025-07-13 13:15:38 +0530

    Update variables.tf

--- terraform-k8s-jenkins/variables.tf
+++ terraform-k8s-jenkins/variables.tf
@@ -1,1 +1,16 @@
+variable "key_name" {
+  default = "my-key"
+}
+
+variable "public_key_path" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "instance_type" {
+  default = "t2.medium"
+}
+
+variable "ami" {
+  default = "ami-007020fd9c84e18c7" # Ubuntu 22.04 in ap-south-1
+}
 
