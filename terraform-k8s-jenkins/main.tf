51c6be7d6281a7c7b381369166daa3b4a0d705b8
Ashu2356 <asavariparit29@gmail.com>
2025-07-13 13:16:37 +0530

    Update main.tf

--- terraform-k8s-jenkins/main.tf
+++ terraform-k8s-jenkins/main.tf
@@ -1,1 +1,61 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = var.key_name
+  public_key = file(var.public_key_path)
+}
+
+resource "aws_security_group" "devops_sg" {
+  name        = "devops-sg"
+
+  ingress = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 6443
+      to_port     = 6443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+
+  egress = [{
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }]
+}
+
+resource "aws_instance" "jenkins" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
+  user_data              = file("jenkins-user-data.sh")
+
+  tags = {
+    Name = "Jenkins-Server"
+  }
+}
+
+resource "aws_instance" "k8s_master" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
+  user_data              = file("kubeadm-user-data.sh")
+
+  tags = {
+    Name = "K8s-Master"
+  }
+}
 
