6515ef0f16ac1ab4256ab97b1e969b29d326efbc
Ashu2356 <asavariparit29@gmail.com>
2025-07-13 13:17:27 +0530

    Update jenkins-user-data.sh

--- terraform-k8s-jenkins/jenkins-user-data.sh
+++ terraform-k8s-jenkins/jenkins-user-data.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+yum update -y
+amazon-linux-extras enable corretto11
+yum install -y java-11-amazon-corretto git docker
+
+systemctl start docker
+systemctl enable docker
+
+usermod -aG docker ec2-user
+
+# Install Jenkins
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+yum install -y jenkins
+systemctl start jenkins
+systemctl enable jenkins
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+mv kubectl /usr/local/bin/
 
